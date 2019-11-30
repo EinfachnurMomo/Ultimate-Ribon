717f872ab57b93a664e135f914f9589599ac5ae0
EinfachNurMomo <normantraumueller@gmx.de>
2019-11-30 17:54:03 +0100

    Added JS for new Home.html

---
+++ js/newhome.js
@@ -0,0 +1,13 @@
+/* jshint esversion: 9 */
+// Funktion zum selektieren von Items
+const selectElement = (s) => document.querySelector(s);
+
+// Öffnet das Menü beim klicken
+selectElement('.open').addEventListener('click', () => {
+    selectElement('.nav-list').classList.add('active');
+});
+
+// Schließt das Menü beim klicken
+selectElement('.close').addEventListener('click', () => {
+    selectElement('.nav-list').classList.add('active');
+});
